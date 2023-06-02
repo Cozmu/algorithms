cef4c298e1849cd144b42a6342ef7e2c24575f3a
Cozmu <www.jorgereis@gmail.com>
2023-06-01 21:34:05 -0300

    feat: primeiro desafio

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    student_accountant = 0
+    if not target_time or type(permanence_period) != list:
+        return None
+    for student in permanence_period:
+        if student[0] <= target_time <= student[1]:
+            student_accountant += 1
+
+    return student_accountant
+
