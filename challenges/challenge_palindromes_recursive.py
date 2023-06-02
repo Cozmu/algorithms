9bde6447ec097c13b45775951eb6ebaf2e295769
Cozmu <www.jorgereis@gmail.com>
2023-06-02 15:28:51 -0300

    feat: adicionando resto da logica do desafio 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if word[low_index] != word[high_index]:
+    if word == '':
+        return False
+    upper_word = word.upper()
+    if upper_word[low_index] != upper_word[high_index]:
         return False
     else:
-        is_palindrome_recursive(word, low_index + 1, high_index - 1)
-        return True
+        if low_index >= high_index:
+            return True
+        else:
+           return is_palindrome_recursive(upper_word, low_index + 1, high_index - 1)
 
-word = 'AMA'
-print(is_palindrome_recursive(word, 0, len(word) - 1))
+palavra = 'ANNA'
+print(is_palindrome_recursive(palavra, 0, len(palavra) - 1))
