2406cb06831bd4f3749b65d22d923a217ec07c51
Cozmu <www.jorgereis@gmail.com>
2023-06-01 23:22:32 -0300

    feat: tentativa do desafio 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word[low_index] != word[high_index]:
+        return False
+    else:
+        is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        return True
+
+word = 'AMA'
+print(is_palindrome_recursive(word, 0, len(word) - 1))
