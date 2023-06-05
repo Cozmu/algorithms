cd9aadcecd0379dba910a9f33fb33eb9f43d569d
Cozmu <www.jorgereis@gmail.com>
2023-06-05 14:17:23 -0300

    test: testando encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert (encrypt_message('message', 9999) == 'egassem')
+    assert (encrypt_message('message', 2) == 'egass_em')
+    assert (encrypt_message('message', 3) == 'sem_egas')
+    with pytest.raises(TypeError, match= 'tipo inválido para key'):
+        encrypt_message('message', '3')
+    with pytest.raises(TypeError, match= 'tipo inválido para message'):
+        encrypt_message(2, 3)
+    
