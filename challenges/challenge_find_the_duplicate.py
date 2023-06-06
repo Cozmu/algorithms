68ef0852813e584be551dc55e5a1a45ff6ab0d00
Cozmu <www.jorgereis@gmail.com>
2023-06-05 22:29:09 -0300

    feat: challenger find the duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,20 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1:
+        return False
+
+    dictionary_of_repeating_numbers = {}
+    for num in nums:
+        if type(num) != int or num < 0:
+            return False
+        if num in dictionary_of_repeating_numbers:
+            dictionary_of_repeating_numbers[num] += 1
+        else:
+            dictionary_of_repeating_numbers[num] = 1
+
+    for num, quantity in dictionary_of_repeating_numbers.items():
+        if quantity > 1:
+           return quantity
+        else:
+            return False
+
+print(find_duplicate([3, 1, 2, 4, 6, 5, 7, 7, 7, 8]))
