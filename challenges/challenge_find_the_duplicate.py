04b13c38cf4d9469b880cde8aaedb5fbe15a3731
Cozmu <www.jorgereis@gmail.com>
2023-06-06 13:32:01 -0300

    feat: completando logica de verificação de repetidos

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,20 +1,9 @@
 def find_duplicate(nums):
     if len(nums) <= 1:
         return False
-
-    dictionary_of_repeating_numbers = {}
     for num in nums:
-        if type(num) != int or num < 0:
+        if type(num) != int or num <= 0:
             return False
-        if num in dictionary_of_repeating_numbers:
-            dictionary_of_repeating_numbers[num] += 1
-        else:
-            dictionary_of_repeating_numbers[num] = 1
-
-    for num, quantity in dictionary_of_repeating_numbers.items():
-        if quantity > 1:
-           return quantity
-        else:
-            return False
-
-print(find_duplicate([3, 1, 2, 4, 6, 5, 7, 7, 7, 8]))
+        if nums.count(num) > 1:
+            return num
+    return False
