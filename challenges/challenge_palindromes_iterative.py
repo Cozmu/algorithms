7bccc20c72fdcce8a415a81482350d52c1dd0cbf
Cozmu <www.jorgereis@gmail.com>
2023-06-06 14:00:39 -0300

    feat: challenge palindromes iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+    upper_word = word.upper()
+    last_char = len(upper_word) - 1
+    for index in range(len(upper_word)//2):
+        if upper_word[index] != upper_word[last_char - index]:
+            return False
+    return True
