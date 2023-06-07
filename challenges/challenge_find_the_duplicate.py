68f3c24d91156f7b075c8051a43bd6fb6b61aa99
Cozmu <www.jorgereis@gmail.com>
2023-06-06 21:13:23 -0300

    refactor: diminuindo complexidade da busca por duplicatas

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,9 +1,11 @@
 def find_duplicate(nums):
     if len(nums) <= 1:
         return False
-    for num in nums:
-        if type(num) != int or num <= 0:
+    nums.sort()
+
+    for index in range(len(nums) - 1):
+        if type(nums[index]) != int or nums[index] <= 0:
             return False
-        if nums.count(num) > 1:
-            return num
+        if nums[index] == nums[index + 1]:
+            return nums[index]
     return False
